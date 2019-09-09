4adecf3edd3fcea7c0a09f6eb038ddb51017a3d1
ssh <949589855@qq.com>
2019-09-09 20:20:53 +0800

    fix: 部署服务器路径修复

--- docs/_next/static/chunks/styles.6a4a434c47b09e3b115e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{KYme:function(n,w,o){}}]);
--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const withCss = require('@zeit/next-css')
 
-// next.config.js增加代码
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = withCss({
-  assetPrefix: isProd ? '/next-blog/' : '',
-})
+module.exports = withCss({})
